6657cb873eaeceb9adf50f22e13083721e1b8383
naon708 <naoya_nis@icloud.com>
2022-04-04 11:47:54 +0900

    Fix: validation of User columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :name, uniqueness: { message: '他のニックネームをお試しください。' }
+  validates :name, length: { in: 6..20 }
   validates :password_digest, presence: true
 end
