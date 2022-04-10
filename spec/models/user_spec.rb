00bda7ed038ac2f0ef71559399e0b998fb46192b
naon708 <naoya_nis@icloud.com>
2022-04-10 18:15:49 +0900

    Fix: Use change_matcher

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'create' do
     it 'is successfull create user' do
-      user = create(:user)
-      expect(User.count).to eq 1
+      expect{ create(:user) }.to change(User, :count).by(1)
     end
   end
 
