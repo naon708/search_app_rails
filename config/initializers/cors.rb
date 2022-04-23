3531b8625e86f3ed5ad27de7dd3f1104746c7b88
naon708 <naoya_nis@icloud.com>
2022-04-23 22:04:44 +0900

    Add: CORS Allow Origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://pinap.jp', 'http://localhost:8000'
+    origins 'https://pinap.jp', 'http://localhost:8000', 'https://search-app-40e51.web.app'
     # origins 'https://pinap.jp'
 
     resource '*',
