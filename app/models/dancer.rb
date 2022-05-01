7faa3e864195d611452e5a1370bdfe7a05ebb63c
naon708 <naoya_nis@icloud.com>
2022-05-01 22:08:37 +0900

    Improve

--- app/models/dancer.rb
+++ app/models/dancer.rb
@@ -4,7 +4,6 @@ class Dancer < ApplicationRecord
   with_options uniqueness: true do
     validates :japanese_notation
     validates :universal_notation, presence: true
-    # validates :russian_notation
     validates :name_order, presence: true
   end
 end
