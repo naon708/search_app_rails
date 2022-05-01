37df119c2d787e5d2a129b1422c50e8af1d532b5
naon708 <naoya_nis@icloud.com>
2022-05-01 22:34:42 +0900

    Add: Column title

---
+++ db/migrate/20220501131514_add_column_title_to_dancers.rb
@@ -0,0 +1,5 @@
+class AddColumnTitleToDancers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :dancers, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_100234) do
+ActiveRecord::Schema.define(version: 2022_05_01_131514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_100234) do
     t.string "name_order", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   create_table "mark_dancers", force: :cascade do |t|
