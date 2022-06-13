cd9bfa28effe4dfa809407bbcd648c91a06aceae
naon708 <naoya_nis@icloud.com>
2022-06-13 12:20:45 +0900

    Fix: allメソッドを除去_#27

--- app/controllers/api/v1/autocompletes_controller.rb
+++ app/controllers/api/v1/autocompletes_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AutocompletesController < ApplicationController
   def index
-    program = Program.all.pluck(:japanese_notation)
-    dancer = Dancer.all.pluck(:title)
-    step = Step.all.pluck(:japanese_notation)
-    variation = Variation.all.pluck(:title)
+    program = Program.pluck(:japanese_notation)
+    dancer = Dancer.pluck(:title)
+    step = Step.pluck(:japanese_notation)
+    variation = Variation.pluck(:title)
     
     titles = []
     titles.push(dancer, step).flatten!.sort!
